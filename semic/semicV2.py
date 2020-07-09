6d19e0cf1906fc8eaa873c6e188fc92e54807e56
Jason <jason.daurat@laposte.net>
2020-07-09 09:55:33 +0200

    Add main function

--- semic/semicV2.py
+++ semic/semicV2.py
@@ -2,9 +2,10 @@ from semic.maps import get_plan
 from semic.meteo import get_historique_meteo, get_meteo, get_meteo_monthly, estimate_meteo_year, find_insee
 from semic.gps_info import get_elevation_fr, get_elevation, get_city, select_city_postal
 from semic.sentinelProcess import search_tile
+import json
 
 class Point:
-    def __init__(self, coord, user, pw, width, size, dist, year:int, month:int = None, day:int = None):
+    def __init__(self, coord, user, pw, width, size, year:int, month:int = None, day:int = None):
         self.coord = coord
         self.lon = coord[0]
         self.lat = coord[1]
@@ -15,7 +16,6 @@ class Point:
         self.pw = pw
         self.width = width
         self.size = size
-        self.dist = dist
         
     def elevation(self):
         return get_elevation_fr(self.coord)
@@ -35,15 +35,40 @@ class Point:
                 return get_meteo(insee_code, date)
     
     def map(self, style = 'plan', w = None, height = None):
-        return get_plan(self.coord, self.dist, style, w, height)
+        return get_plan(self.coord, self.width, style, w, height)
     
-    def get_sentinel_im(self, l=1, p='./',tile_name=None):
+    def get_sentinel_im(self, l=1, p='./', tile_name=None):
         date = (str(self.year)+'-'+"{0:0=2d}".format(self.month)+'-'+"{0:0=2d}".format(self.day)+'T00:00:00Z-10DAYS', 
                 str(self.year)+'-'+"{0:0=2d}".format(self.month)+'-'+"{0:0=2d}".format(self.day)+'T00:00:00Z')
         im = search_tile(self.user, self.pw, date, self.coord, self.width, l, p, tile_name)
         if im != None :
             im = im.resize(self.size)
             return(im)
+    
+    def all_infos(self, l = 1, p = './', tile_name = None, path = None, save = False, sort = True):
+        dic = self.weather()
+        img_plan = self.map()
+        img_sat = self.map(style = 'sat')
+        img_sentinel = self.get_sentinel_im(l, p, tile_name)
+        
+        if save == True:
+            img_plan.save(path + 'img_plan.jpg', 'JPEG')
+            img_sat.save(path + 'img_sat.jpg', 'JPEG')
+            img_sentinel.save(path + 'img_sentinel.jpg', 'JPEG')
+            dic['img_plan'] = path + 'img_plan.jpg'
+            dic['img_sat'] = path + 'img_sat.jpg'
+            dic['img_sentinel'] = path + 'img_sentinel.jpg'
+            with open(path + dic['ville'] + '_' + str(self.year) + '.json', 'w') as fp:
+                json.dump(dic, fp, sort_keys=sort, indent=4)
+        else:
+            dic['img_plan'] = img_plan
+            dic['img_sat'] = img_sat
+            dic['img_sentinel'] = img_sentinel
+        return dic
+        
+        
+        
+
 
 class Line:
     def __init__(self, coords, user, pw, width, size, dist, year:int, month:int = None, day:int = None):
